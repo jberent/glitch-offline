310342ca9088dd5ff14f9d0318de3edf6a4ce871
jonathan berent <jonathan.berent@dx-fx.com>
2021-10-13 18:44:51 -0400

    add arena

---
+++ src/arena.ts
@@ -0,0 +1,31 @@
+
+
+class Builder {
+    public Build() {
+
+    }
+
+    public Render(){
+
+    }
+}
+
+/*
+    from Red Alliance, x is right, y is away
+*/
+class Arena {
+    private widthTiles: number = 6;
+    private heightTiles: number = 6;
+
+    private tiles : Tile[][] = null; 
+    public Build():void {
+
+    }
+
+}
+
+class Tile {
+    private widthInches = 23.5;
+    private heightInches = 23.5;
+
+}
